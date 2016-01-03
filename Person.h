1a367afdaf04ade997dc2d959fb7137f7d7043bb
Nada-diaelsaid <nada_diaelsaid@hotmail.com>
2016-01-03 13:10:03 +0200

    I just copied it to my project
    NOO CHANGES

--- Person.h
+++ Person.h
@@ -2,7 +2,8 @@
  * Person.h
  *
  *  Created on: Dec 24, 2015
- *      Author: abdulaziz & Nada
+ *  Author: abdulaziz & Nada
+ *
  */
 
 #ifndef PERSON_H_
@@ -11,20 +12,34 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <stdlib.h> //atoi() , 
+#include <fstream>
+#include <utility>
+#include <limits>
 
 using namespace std;
 
 
 class Person {
 public:
-	Person(string name, string title, string company, string address);
-	virtual ~Person();
-//private:
+	///////////////////// Constructor and distructor //////////////
+	Person(string name, string title, string company, string address); // our constuctor
+	
+	/** distructor **/
+	virtual ~Person(); 
+
+	////////////  Defining Variables //////////////
+
+	/** Person name **/
 	string name;
-	string title;
-	string company;
-	string address;
-	vector<Person *> connection;
+	/**person job title **/
+	string title; 
+	/** company he works in **/
+	string company; 
+	/** person address **/
+	string address; 
+	/** Connections that person have , it is a vector of pointers to persons **/
+	vector<Person *> connection; 
 
 };
 
